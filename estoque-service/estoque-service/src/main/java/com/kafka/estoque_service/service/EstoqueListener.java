b01a08e856e8fe4f52ac39519c7b2f2f5130fe62
thiago.cordeiro <thiago.cordeiro@mv.com.br>
2026-03-10 11:25:39 -0300

    adicionando retentativas ao consumer

--- estoque-service/estoque-service/src/main/java/com/kafka/estoque_service/service/EstoqueListener.java
+++ estoque-service/estoque-service/src/main/java/com/kafka/estoque_service/service/EstoqueListener.java
@@ -1,11 +1,19 @@
 package com.kafka.estoque_service.service;
 
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EstoqueListener {
     @KafkaListener(topics = "estoque-topico", groupId = "estoque-group")
+    @RetryableTopic(
+            backOff =   @BackOff(value = 3000),
+            attempts = "5",
+            autoCreateTopics = "true",
+            include = Exception.class
+    )
     public void processarVenda(String mensagem) {
         System.out.println("Venda recebida: " + mensagem);
     }
